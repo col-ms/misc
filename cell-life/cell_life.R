59c02ddcd5ecb0883bced1b87b70ebf676b466ec
Collin Smith <collinmsmith22@gmail.com>
2023-01-03 17:20:46 -0500

    Convert to callable function

--- cell-life/cell_life.R
+++ cell-life/cell_life.R
@@ -1,102 +1,77 @@
-# Define a function that returns the number of active cells
-# that neighbour a cell in a specified position.
-cnt.nbrs <- function(x, i, j){
+cycle <- function(cells, steps = 10, path = NULL,
+                  save.plots = FALSE, check.orig = 'f', return = FALSE,
+                  colors = c("darkblue", "orange"), sleep.time = 0.75){
   
-  # If cell is not at the edge of the matrix,
-  # return the sum of all 8 neighbouring cells.
-  if(i < nrow(x) & j < ncol(x)){
-    return(sum(c(x[i-1, j-1],
-                 x[i-1, j],
-                 x[i-1, j+1],
-                 x[i, j-1],
-                 x[i, j+1],
-                 x[i+1, j-1],
-                 x[i+1, j],
-                 x[i+1, j+1])))
-  }
+  source("cntNeighbours.R")
+  #if(check.orig != 'f') orig <- cells
+  if(save.plots) source("utils.R")
   
-  # If cell is on bottom row of matrix,
-  # return sum of 5 neighbouring cells.
-  else if(i == nrow(x) & j < ncol(x)){
-    return(sum(c(x[i-1, j-1],
-                 x[i-1, j],
-                 x[i-1, j+1],
-                 x[i, j-1],
-                 x[i, j+1])))
-  }
-    
-  # If cell is on right edge of matrix,
-  # return sum of 5 neighbouring cells.
-  else if(i < nrow(x) & j == ncol(x)){
-    return(sum(c(x[i-1, j-1],
-                 x[i-1, j],
-                 x[i, j-1],
-                 x[i+1, j-1],
-                 x[i+1, j])))
-  }
-    
-  # If cell is on the bottom right corner of matrix,
-  # return sum of 3 neighbouring cells.
-  else if(i == nrow(x) & j == ncol(x)){
-    return(sum(c(x[i-1, j-1],
-                 x[i-1, j],
-                 x[i, j-1])))
-  }
-}
-
-# Initialize empty matrix of cells.
-cells <- matrix(0, nrow = 101, ncol = 101)
-  
-# Set starting locations for active cells.
-# Initial pattern is as follows:
-  
-# . . . . .
-# . 1 1 1 .   '1' = active cell
-# . 1 . 1 .   '.' = inactive cell
-# . 1 1 1 .
-# . . . . .
-
-# Note, this pattern is centered in the matrix.
-cells[50, 50:52] = cells[51, c(50, 52)] = cells[52, 50:52] = 1
-
-# Display initial matrix state.
-# Active cells are colored oragnge,
-# inactive cells are colored dark blue.
-image(cells, col = c("darkblue", "orange"), 
-      asp = 1, bty = "n", xaxt = "n", yaxt = "n", main = "1")
-
-# Initialize step counter.
-step = 1
-
-# Begin repeat loop. 
-# Note: loop continues until manual termination from user.
-repeat{
-  
-  # Create a copy of the original matrix state.
-  copymat = cells
-  
-  # Iterate over every row in matrix.
-  for(i in 1:nrow(cells)){
-    
-    # Iterate over every column in matrix.
-    for(j in 1:ncol(cells)){
-      
-      # If current cell has an odd number of active 
-      # neighbours, set current cell to active.
-      # Otherwise, set current cell to inactive.
-      cells[i, j] = ifelse(
-        (cnt.nbrs(copymat, i, j) %% 2 != 0),
-        1, 0)
+  drawPlot <- function(){
+    if(!save.plots){
+      image(z=cells, col = colors,
+            asp = 1, main = step,
+            bty = 'n', xaxt = 'n', yaxt = 'n')
+    }
+    else if(save.plots){
+      png(fnamegen(step = step, path = path))
+      image(z=cells, col = colors,
+            asp = 1, main = step,
+            bty = 'n', xaxt = 'n', yaxt = 'n')
+      dev.off()
     }
   }
   
-  # Increment step counter by 1.
-  step = step + 1
+  step <- 1
   
-  # Display updated matrix state, using same colors as before.
-  image(cells, col = c("darkblue", "orange"), 
-        asp = 1, bty = "n", xaxt = "n", yaxt = "n", main = step)
+  drawPlot()
   
-  # Short wait time before next cycle.
-  Sys.sleep(0.75)
+  for(i in 1:(steps-1)){
+    
+    # Sleep time before next cycle (defaults to 0.75).
+    Sys.sleep(sleep.time)
+    
+    copymat = cells
+    
+    # Iterate over every row in matrix.
+    for(i in 1:nrow(cells)){
+      
+      # Iterate over every column in matrix.
+      for(j in 1:ncol(cells)){
+        
+        # If current cell has an odd number of active 
+        # neighbours, set current cell to active.
+        # Otherwise, set current cell to inactive.
+        cells[i, j] = ifelse(
+          (cnt.nbrs(copymat, i, j) %% 2 != 0),
+          1, 0)
+      }
+    }
+    
+    
+    # see TODO below
+    # if(check.orig != 'f' && i == 1) orig <- cells
+    
+    # Increment step counter.
+    step = step + 1
+    
+    # Display updated matrix state, using same colors as before.
+    drawPlot()
+    
+    
+    # TODO: get original comparison working
+    # if(check.orig != 'f'){
+    #   if(all(orig == cells) && check.orig == 'n'){ 
+    #     cat("Matrix has returned to original state at step #", step, sep = '')
+    #   }
+    #   else if(all(orig == cells) && check.orig == 'r'){
+    #     cat("Matrix has returned to original state at step #", step, sep = '')
+    #     return(step)
+    #   }
+    # }
+    
+
+  }
+  if(return) return(cells)
+  return(-1)
 }
+
